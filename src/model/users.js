4e2b4585013c5d3b63df2bb8ad54a7af923439ff
luojinxu520 <you@example.com>
2021-01-03 17:22:44 +0800

    update: 调整部分用户模型数据类型为引用类型

--- src/model/users.js
+++ src/model/users.js
@@ -19,31 +19,34 @@ const userSchema = new Schema({
     name: { type: String, required: true},
     password: { type: String, required: true, select: true},
     phone: { type: String, default: null, select: false },
-    acatar_url: { type: String },
+    acatar_url: { type: String, select: false },
     gender: { type: String, enum: ["male", "female"], default: "male", select: false },
     headline: { type: String, select: false },
-    locations: { type: [{type: String}], select: false },
-    business: { type: String, select: false },
+    locations: { type: [{type: Schema.Types.ObjectId, ref: "Topic" }], select: false },
+    business: { type: Schema.Types.ObjectId, ref: "Topic", select: false },
     employments: { 
         type: [{
-            company: { type: String },
-            job: { type: String }
+            company: { type: Schema.Types.ObjectId, ref: "Topic" },
+            job: { type: Schema.Types.ObjectId, ref: "Topic" }
         }], 
         select: false
     },
     educations: {
         type: [{
-            school: { type: String },
-            major: { type: String },
+            school: { type: Schema.Types.ObjectId, ref: "Topic" },
+            major: { type: Schema.Types.ObjectId, ref: "Topic" },
             diploma: { type: Number, enum: [1,2,3,4,5] },
             entrance_year: { type: Number },
             graduation_year: { type: Number }
         }],
         select: false
     },
-    // Schema.Types.ObjectId 是由mongoose提供的特殊ID数据类型，用于存储id, ref用于后期引用
     following: {
         type: [{ type: Schema.Types.ObjectId, ref: "User"}],
+        select: false
+    },
+    followingTopics: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Topic" }],
         select: false
     }
 })
